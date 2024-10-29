7436aff65f6fa3c328865bfbfa2d849f369ad5e5
Ismail Marghich <ismailcodes@gmail.com>
2024-10-29 01:13:14 +0100

    actually finished part 2 b

---
+++ Part 2/courseinfo2/src/components/Course.jsx
@@ -0,0 +1,32 @@
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((acc, current) => (acc += current.exercises), 0);
+  return <p>Number of exercises {sum}</p>;
+};
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
+    </div>
+  );
+};
+
+export default Course;
